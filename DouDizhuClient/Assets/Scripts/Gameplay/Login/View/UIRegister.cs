91a98ca15454a32de617e6c626ac601312bd1759
MengZhangxi <zhangxi.meng@centurygame.com>
2025-07-23 13:03:31 +0800

    登录、注册界面文本设置，界面布局修改，注册时显示提示

--- DouDizhuClient/Assets/Scripts/Gameplay/Login/View/UIRegister.cs
+++ DouDizhuClient/Assets/Scripts/Gameplay/Login/View/UIRegister.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using TMPro;
 using UIModule;
 using Gameplay.Player.Service;
+using UnityEngine.UI;
+using Config;
 
 namespace Gameplay.Login.View
 {
@@ -12,7 +14,34 @@ namespace Gameplay.Login.View
         [SerializeField]
         private TMP_InputField m_AccountInput;
         [SerializeField]
+        private TextMeshProUGUI m_TextAccountTip;
+        [SerializeField]
         private TMP_InputField m_PasswordInput;
+        [SerializeField]
+        private TextMeshProUGUI m_TextPasswordTip;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            m_AccountInput.onSelect.AddListener(OnSelectAccountInput);
+            m_AccountInput.onDeselect.AddListener(OnDeselectAccountInput);
+            m_PasswordInput.onSelect.AddListener(OnSelectPasswordInput);
+            m_PasswordInput.onDeselect.AddListener(OnDeselectPasswordInput);
+
+            m_TextAccountTip.gameObject.SetActive(false);
+            m_TextPasswordTip.gameObject.SetActive(false);
+        }
+
+        private void Start()
+        {
+            int accountMinLength = ConfigsManager.Instance.GetConst<int>("AccountMinLength");
+            int accountMaxLength = ConfigsManager.Instance.GetConst<int>("AccountMaxLength");
+            int passwordMinLength = ConfigsManager.Instance.GetConst<int>("PasswordMinLength");
+            int passwordMaxLength = ConfigsManager.Instance.GetConst<int>("PasswordMaxLength");
+            m_TextAccountTip.text = StringsHelper.GetString("AccountTip", accountMinLength, accountMaxLength);
+            m_TextPasswordTip.text = StringsHelper.GetString("PasswordTip", passwordMinLength, passwordMaxLength);
+        }
 
         public async Task OnClickConfirmAsync()
         {
@@ -27,6 +56,26 @@ namespace Gameplay.Login.View
         private async void OnClickConfirm()
         {
             await OnClickConfirmAsync();
+        }
+
+        private void OnSelectAccountInput(string _)
+        {
+            m_TextAccountTip.gameObject.SetActive(true);
+        }
+
+        private void OnDeselectAccountInput(string _)
+        {
+            m_TextAccountTip.gameObject.SetActive(false);
+        }
+
+        private void OnSelectPasswordInput(string _)
+        {
+            m_TextPasswordTip.gameObject.SetActive(true);
+        }
+
+        private void OnDeselectPasswordInput(string _)
+        {
+            m_TextPasswordTip.gameObject.SetActive(false);
         }
     }
 }
