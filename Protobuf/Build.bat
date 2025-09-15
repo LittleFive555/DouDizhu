aefe0dc8a76461aac33fe0d9d2b1e79bbf24bfa9
MengZhangxi <zhangxi.meng@centurygame.com>
2025-09-15 18:43:09 +0800

    修改protobuf批处理文件

--- Protobuf/Build.bat
+++ Protobuf/Build.bat
@@ -1,4 +1,4 @@
 del /s /q "..\DouDizhuClient\Assets\Scripts\Network\Proto\*"
-del /s /q "..\DouDizhuServer\network\protodef\*"
-protoc --proto_path=./ --proto_path=./include --csharp_out=../DouDizhuClient/Assets/Scripts/Network/Proto --go_out=../DouDizhuServer/ *.proto
+del /s /q "..\DouDizhuServer\scripts\network\protodef\*"
+protoc --proto_path=./ --proto_path=./include --csharp_out=../DouDizhuClient/Assets/Scripts/Network/Proto --go_out=../DouDizhuServer/scripts/ *.proto
 pause
